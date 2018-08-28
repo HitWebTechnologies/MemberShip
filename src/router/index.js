455a753fa130a3189efc032adb87329ad54ecf50
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-08-28 23:31:47 +0200

    Add the registration page to the project
    -- this is where the members are going to input theirdetails

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/pages/HelloWorld'
+import RegisterAccount from '@/pages/RegisterAccount'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'RegisterAccount',
+      component: RegisterAccount
     }
   ]
 })
