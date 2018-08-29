a77a249a25ab8e772f0fa1a8f0fd0f58f7a40e87
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-08-29 07:14:35 +0200

    Create a skeleton Main Navigation bar
    -- will add functionality to it later

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
-    <span class="mr-4">A very basic setup of <a href="https://vuejs.org" class="text-green">vue</a> with <a href="https://tailwindcss.com" class="text-green">tailwind</a>.</span>
-    <button class="py-4 px-6 bg-green text-white shadow-lg rounded" @click="$router.push('/register')">Proceed to registration</button>
+  <div class="border-t-4 border-green">
+    <!-- The app navigatioinn bar -->
+    <div class="bg-white border-b">
+      <div class="container mx-auto flex justify-between items-center ">
+        <a href="#" @click="$router.push('/')" class="flex justify-center py-4 no-underline text-grey-darkest">
+          <!-- A fancy makeshift icon. Replace with actual icon you will design -->
+            <span class="bg-green border-2 border-green text-white  py-1 px-2  rounded-l m-0">HIT</span>
+            <span class="border-2 border-green py-1 px-2 rounded-r m-0">WebTech</span> 
+        </a>
+
+        <a href="#" class="flex items-center p-4 no-underline text-grey-darkest hover:bg-green-lightest">
+          <div class="w-8 h-8 rounded-full bg-green-light mr-2">&nbsp;</div>
+          <span>Kudakwashe Paradzayi</span>
+        </a>
+      </div>
+    </div>
+
+
+    <!-- where the main app routes will be mounted -->
+    <div class="h-screen flex items-center justify-center">
+      <span class="mr-4">A very basic setup of <a href="https://vuejs.org" class="text-green">vue</a> with <a href="https://tailwindcss.com" class="text-green">tailwind</a>.</span>
+      <button class="py-4 px-6 bg-green text-white shadow-lg rounded" @click="$router.push('/register')">Proceed to registration</button>
+    </div>
   </div>
 </template>
 
