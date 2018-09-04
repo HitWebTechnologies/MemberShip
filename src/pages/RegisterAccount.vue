61ccf507995ead51f235739ac0b2d58c394e3ad0
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-04 09:40:02 +0200

    Adjust the look of the form controls and center them
    -- remove box shadow from buttons
        remove social login - not yet functional
       reduce padding on the inputs

--- src/pages/RegisterAccount.vue
+++ src/pages/RegisterAccount.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="border-t-4 border-green h-screen flex justify-center items-center">
     <div class="container mx-auto py-8">
-      <h3 class="mb-6 font-light text-4xl">Welcome to HIT Web Technologies</h3>
 
-      <div class="bg-white p-6 rounded ">
+      <div class="form-area mx-auto bg-white p-6 rounded ">
+      <h3 class="mb-6 pb-4 font-light text-2xl border-b">Welcome to HIT Web Technologies</h3>
 
         <div class="flex mb-6">
           <!-- Full name -->
           <div class="w-3/5 mr-4">
             <label for="name" class="block mb-2 font-medium">Enter your full name</label>      
-            <input id="name" name="name" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="John Doe">
+            <input id="name" name="name" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="John Doe">
           </div>
 
           <!-- Reg number -->
           <div class="flex-1">
             <label for="regNumber" class="block mb-2 font-medium">Enter Reg Number</label>
-            <input id="regNumber" name="regNumber" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="H181234A">
+            <input id="regNumber" name="regNumber" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="H181234A">
           </div>
         </div>
 
@@ -23,13 +23,13 @@
           <!-- Reg number -->
           <div class="flex-1 mr-4">
             <label for="phoneNumber" class="block mb-2 font-medium">Enter Phone Number</label>
-            <input id="phoneNumber" name="phoneNumber" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="0717123456">
+            <input id="phoneNumber" name="phoneNumber" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="0717123456">
           </div>
 
           <!-- email -->
           <div class="flex-1">
             <label for="email" class="block mb-2 font-medium">Enter Email Address</label>
-            <input id="email" name="email" type="email" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="john.doe@email.com">
+            <input id="email" name="email" type="email" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="john.doe@email.com">
           </div>
         </div>
 
@@ -37,7 +37,7 @@
           <!-- Year -->
           <div class="flex-1 mr-4">
             <label for="year" class="block mb-2 font-medium">Enter Year</label>
-            <select id="year" name="year" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="H181234A">
+            <select id="year" name="year" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="H181234A">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -47,7 +47,7 @@
 
           <div class="flex-1">
             <label for="year" class="block mb-2 font-medium">What program?</label>
-            <select id="year" name="year" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="H181234A">
+            <select id="year" name="year" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="H181234A">
               <option value="1">Software Engineering</option>
               <option value="2">IT</option>
               <option value="3">CompScience</option>
@@ -56,7 +56,11 @@
           </div>
         </div>
 
-        <button @click="$router.push('/register-login')" class="block w-64 py-4 px-6 bg-green text-white shadow-md hover:shadow-lg rounded">Create my account</button>
+        <div class="mb-6">
+          <label for="twitter" class="block mb-2 font-medium">Twitter</label>
+          <input id="twitter" name="twitter" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="@kudapara">
+        </div>
+        <button @click="$router.push('/register-login')" class="block w-full py-4 px-6 bg-green text-white shadow-md- hover:bg-green-dark rounded">Create my account</button>
 
       </div>
 
@@ -71,5 +75,7 @@ export default {
 </script>
 
 <style>
-
+  .form-area {
+    max-width: 600px;
+  }
 </style>
--- src/pages/RegisterAccountLogin.vue
+++ src/pages/RegisterAccountLogin.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="border-t-4 border-green h-screen flex justify-center items-center">
     <div class="container mx-auto py-8">
-      <h3 class="mb-6 font-light text-4xl">Login the way you want</h3>
-      
-      <div class="flex bg-white p-6 rounded">
-        <!-- manual login -->
-        <div class="flex-1 border-r px-6">
+      <div class="form-area mx-auto lflex bg-white p-6 rounded">
+      <h3 class="mb-6 pb-4 font-light text-2xl border-b">Signup the way you want</h3>
+        <!-- social Signup
+        comment it out. we dont need it yet.
+        <div class="w-full flex items-center justify-between pb-4 border-b mb-6">
+          <button class="block w-1/2-almost py-4 px-6 bg-white text-blue border-2 hover:bg-grey-lightest rounded">Signup with Google</button>
+          <button class="block w-1/2-almost py-4 px-6 bg-blue text-white border-2 border-transparent hover:bg-blue-dark rounded">Signup with Facebook</button>
+        </div>
+         -->
+        <!-- manual Signup -->
+        <div>
           <div class="mb-6">
             <label for="username" class="block mb-2 font-medium">Enter your username</label>      
-            <input id="username" name="username" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="JohnDoe">
+            <input id="username" name="username" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="JohnDoe">
           </div>
 
           <div class="mb-6">
             <label for="password" class="block mb-2 font-medium">Enter your password</label>      
-            <input id="password" name="password" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="***">
+            <input id="password" name="password" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="***">
           </div>
 
           <div class="mb-6">
             <label for="confirmPassword" class="block mb-2 font-medium">Confirm your password</label>      
-            <input id="confirmPassword" name="confirmPassword" type="text" class="w-full bg-grey-lightest p-4 rounded border-2 border-green-lightest" placeholder="***">
+            <input id="confirmPassword" name="confirmPassword" type="text" class="w-full bg-grey-lightest p-3 rounded border-2 border-green-lightest" placeholder="***">
           </div>
 
-          <button class="block w-64 py-4 px-6 bg-green text-white shadow-md hover:shadow-lg rounded">Create my account</button>
+          <button class="block w-full py-4 px-6 bg-green text-white hover:bg-green-dark rounded">Create my account</button>
         </div>
 
-        <!-- social login -->
-        <div class="flex-1 flex flex-col justify-center items-center">
-          <button class="block w-64 py-4 px-6 bg-white text-blue shadow-md hover:shadow-lg rounded mb-6">Login with Google</button>
-          <button class="block w-64 py-4 px-6 bg-blue text-white shadow-md hover:shadow-lg rounded">Login with Facebook</button>
-        </div>
       </div>
     </div>
   </div>
