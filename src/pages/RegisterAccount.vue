461dbb514869ff235510aab1ae70e611feaa1cef
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-05 15:58:43 +0200

    Allow the app to create an account
    -- this is the first functional part of registering
       an account. Add more in the next commits

--- src/pages/RegisterAccount.vue
+++ src/pages/RegisterAccount.vue
@@ -9,13 +9,13 @@
           <!-- Full name -->
           <div class="w-3/5 mr-4">
             <label for="name" class="block mb-2 font-medium">Enter your full name</label>      
-            <input id="name" name="name" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="John Doe">
+            <input v-model="account.fullName" id="name" name="name" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="John Doe">
           </div>
 
           <!-- Reg number -->
           <div class="flex-1">
             <label for="regNumber" class="block mb-2 font-medium">Enter Reg Number</label>
-            <input id="regNumber" name="regNumber" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
+            <input v-model="account.regNumber" id="regNumber" name="regNumber" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
           </div>
         </div>
 
@@ -23,13 +23,13 @@
           <!-- Reg number -->
           <div class="flex-1 mr-4">
             <label for="phoneNumber" class="block mb-2 font-medium">Enter Phone Number</label>
-            <input id="phoneNumber" name="phoneNumber" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="0717123456">
+            <input v-model="account.phoneNumber" id="phoneNumber" name="phoneNumber" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="0717123456">
           </div>
 
           <!-- email -->
           <div class="flex-1">
             <label for="email" class="block mb-2 font-medium">Enter Email Address</label>
-            <input id="email" name="email" type="email" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="john.doe@email.com">
+            <input v-model="account.emailAddress" id="email" name="email" type="email" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="john.doe@email.com">
           </div>
         </div>
 
@@ -37,7 +37,7 @@
           <!-- Year -->
           <div class="flex-1 mr-4">
             <label for="year" class="block mb-2 font-medium">Enter Year</label>
-            <select id="year" name="year" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
+            <select v-model="account.level" id="year" name="year" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -47,20 +47,19 @@
 
           <div class="flex-1">
             <label for="year" class="block mb-2 font-medium">What program?</label>
-            <select id="year" name="year" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
-              <option value="1">Software Engineering</option>
-              <option value="2">IT</option>
-              <option value="3">CompScience</option>
-              <option value="4">Food Processing Technologies</option>
+            <select v-model="account.degreeProgram" id="year" name="year" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="H181234A">
+              <option v-for="program in degreePrograms" :value="program.code" :key="program.code">
+                {{ program.title }}
+              </option>
             </select>
           </div>
         </div>
 
         <div class="mb-6">
           <label for="twitter" class="block mb-2 font-medium">Twitter</label>
-          <input id="twitter" name="twitter" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="@kudapara">
+          <input v-model="account.twitterHandle" id="twitter" name="twitter" type="text" class="w-full bg-white p-3 rounded border-2 border-grey-light" placeholder="@kudapara">
         </div>
-        <button @click="$router.push('/register-login')" class="block w-full py-4 px-6 bg-green text-white shadow-md- hover:bg-green-dark rounded">Create my account</button>
+        <button @click="registerAccount()" class="block w-full py-4 px-6 bg-green text-white shadow-md- hover:bg-green-dark rounded">Create my account</button>
 
       </div>
 
@@ -69,8 +68,42 @@
 </template>
 
 <script>
+import axios from '@/libraries/axios'
 export default {
+  data () {
+    return {
+      account: {
+        fullName: '',
+        regNumber: '',
+        phoneNumber: '',
+        emailAddress: '',
+        level: '',
+        degreeProgram: 'ISE',
+        twitterHandle: '',
+      },
 
+      degreePrograms: [
+        { code: 'ISE', title: 'Software Engineering' },
+        { code: 'ICS', title: 'Computer Science' },
+        { code: 'ISA', title: 'Information Security And Assuarance' },
+        { code: 'IIT', title: 'Information Technology'}
+      ]
+    }
+  },
+  methods: {
+    registerAccount () {
+      axios.post('/register', {
+        ...this.account
+      })
+        .then(res => {
+          console.log(res.data)
+          this.$router.push('/register-login')
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
 
