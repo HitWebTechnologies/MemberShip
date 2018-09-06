31b55c24db591e074f8422898b3f3ef47e1aca85
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-06 19:58:57 +0200

    Update the look and feal of the table
    -- add some padding to the table to give
       it some breating space.
       Also wrap the img element in a div
      to make the rounded-full bug go away

--- src/pages/ClubMembers.vue
+++ src/pages/ClubMembers.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container mx-auto pt-8">
     <h3 class="font-medium mb-4">Club members <span class="bg-green rounded-full px-4 text-white">7</span></h3>
-    <div class="bg-white rounded overflow-hidden shadow mb-4">
+    <div class="bg-white rounded overflow-hidden shadow px-4 mb-4">
       <table class="w-full min-w-full rounded">
         <thead>
-          <th class="p-4 bg-white border-b border-grey text-left font-medium">Name</th>
-          <th class="p-4 bg-white border-b border-grey text-left font-medium">Program & Year</th>
-          <th class="p-4 bg-white border-b border-grey text-left font-medium">Twitter Handle</th>
+          <th class="p-4 bg-white border-b-2 border-grey-light text-left font-medium">Name</th>
+          <th class="p-4 bg-white border-b-2 border-grey-light text-left font-medium">Program & Year</th>
+          <th class="p-4 bg-white border-b-2 border-grey-light text-left font-medium">Twitter Handle</th>
         </thead>
         <tbody>
           <tr v-for="n in 7" :key="n">
             <td class="px-4 py-3 border-b border-grey-lighter text-left">
               <div class="flex items-center">
-                <img class="h-8 w-8 bg-grey-light rounded-full mr-2">
+                <div class="h-8 w-8 rounded-full overflow-hidden mr-2">
+                  <img class="h-full w-full bg-grey">
+                </div>
                 <div class="flex flex-col">
                   <span class="font-medium ">Kudakwashe</span>
                   <span class="text-grey-dark">President</span>
