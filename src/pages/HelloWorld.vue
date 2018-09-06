ffd96b2e12b4a99a61b6bfcf92ae442170292008
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-06 19:55:17 +0200

    Update the look and feel of the primary navigation to make it light
    -- Its taking too much attenction form the
      body of the app

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="">
     <!-- The app navigatioinn bar -->
-    <div class="bg-purple border-b">
+    <div class="bg-white border-b">
       <div class="container mx-auto flex justify-between items-center ">
-        <a href="#" @click="$router.push('/')" class="flex justify-center py-4 no-underline text-white">
+        <a href="#" @click="$router.push('/')" class="flex justify-center py-4 no-underline">
           <!-- A fancy makeshift icon. Replace with actual icon you will design -->
             <span class="bg-green border-2 border-green text-white  py-1 px-2  rounded-l m-0">HIT</span>
-            <span class="border-2 border-green py-1 px-2 rounded-r m-0">WebTech</span> 
+            <span class="border-2 border-green py-1 px-2 text-green rounded-r m-0">WebTech</span> 
         </a>
 
         <div class="flex items-center">
-          <a href="#" @click="$router.push('/constitution')" class="border-b-2 border-transparent p-4 no-underline text-white hover:bg-grey-darkest rounded">
+          <a href="#" @click="$router.push('/constitution')" class="border-b-2 border-transparent p-4 no-underline hover:bg-grey-lighter rounded">
             Constitution
           </a>
 
-          <a href="#" class="flex items-center p-4 no-underline text-white hover:bg-grey-darkest rounded">
+          <a href="#" class="flex items-center p-4 no-underline hover:bg-grey-lighter rounded">
             <div class="w-6 h-6 rounded-full bg-green-light mr-2">&nbsp;</div>
             <span>Kudakwashe</span>
           </a>
@@ -23,7 +23,7 @@
     </div>
 
     <!-- A secondary nav bar -->
-    <div class="bg-white border-b">
+    <div class="bg-white- bg-grey-lightest border-b">
       <div class="flex container mx-auto">
         <a href="#" @click="$router.push('/')" :class="{'border-green': $route.name === 'Home'}" class="border-b-2 border-transparent p-4 no-underline text-grey-darkest uppercase tracking-wide font-medium">
           Home
