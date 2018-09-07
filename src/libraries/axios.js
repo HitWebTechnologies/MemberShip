be4904774b8aa150a60a3c14b207d835e75f566c
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-07 11:12:00 +0200

    Use the productioni server
    -- in the future make this a conditional
      so that duting development it uses localhost
      and during production it uses hwt-backend

--- src/libraries/axios.js
+++ src/libraries/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000' // 'http://localhost:3000/api/v1'
+  baseURL: 'https://hwt-backend.now.sh' // 'http://localhost:3000/api/v1'
 })
 
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
