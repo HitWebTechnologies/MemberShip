72b573552f3b92ea9ae46bdf28b285cb5d37a3fb
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-08 20:53:53 +0200

    Let the responsibility of positioning the alert be for the calling code

---
+++ src/components/AppAlertBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-orange-lightest p-6 mb-4 flex items-center justify-between border-l-4 border-orange-dark" v-if="error.show">
+    <div class="text-orange-dark">
+      <h3 class="mb-2 font-medium">Error</h3>
+      <span>{{ this.error.text }}</span>
+    </div>
+    <button @click="$emit('clearError')" class="bg-white text-orange-dark h-10 w-10 rounded shadow">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style>
+
+</style>
